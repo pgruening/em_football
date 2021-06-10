c68789ed0d6f38f6967202b409cc22de82748a35
grueningp <pg@prcmail.de>
2021-06-10 13:58:42 +0200

    single vector output

--- dataset/ds_qualification.py
+++ dataset/ds_qualification.py
@@ -7,7 +7,7 @@ import torch
 ONE_HOT_DIM = 55
 
 
-def get_data_loader(batch_size, num_workers, *, is_train, validation_after_date):
+def get_data_loader(batch_size, num_workers, *, is_train, validation_after_date, shuffle=None):
     data = pd.read_csv('qualifying.csv')
     to_one_hot = ToOneHot(data)
 
@@ -19,27 +19,43 @@ def get_data_loader(batch_size, num_workers, *, is_train, validation_after_date)
     else:
         data = data[data['time'] >= thres]
 
+    if shuffle is not None:
+        shuffle = is_train
+
     return DataLoader(
-        Matches(data, to_one_hot),
+        Matches(data, to_one_hot, switch_teams=is_train),
         batch_size=batch_size, num_workers=num_workers
     )
 
 
+def get_one_hot():
+    data = pd.read_csv('qualifying.csv')
+    to_one_hot = ToOneHot(data)
+    return to_one_hot
+
+
 class Matches(Dataset):
-    def __init__(self, data, to_one_hot):
+    def __init__(self, data, to_one_hot, switch_teams=False):
         self.data = data
         self.toh = to_one_hot
+        self.st = switch_teams
 
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
-        x1 = self.toh(row['team_1'])
-        x2 = self.toh(row['team_2'])
+        if self.st and np.random.rand() < .5:
+            x1 = self.toh(row['team_1'])
+            x2 = self.toh(row['team_2'])
+
+            y1 = row['g1']
+            y2 = row['g2']
+        else:
+            x1 = self.toh(row['team_2'])
+            x2 = self.toh(row['team_1'])
+
+            y1 = row['g2']
+            y2 = row['g1']
 
         x = np.concatenate([x1, x2])
-
-        y1 = row['g1']
-        y2 = row['g2']
-
         return torch.Tensor(x).float(), torch.Tensor([y1, y2]).long()
 
     def __len__(self):
@@ -56,6 +72,9 @@ class ToOneHot():
         out = np.zeros(self.n)
         out[self.teams_.index(team)] = 1.
         return out
+
+    def get_team(self, idx):
+        return self.teams_[idx]
 
 
 def to_time(row):
