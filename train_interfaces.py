047ea81f5bc53f1a70ba92c027dbd0d022d305eb
grueningp <pg@prcmail.de>
2021-06-10 13:58:04 +0200

    update

--- train_interfaces.py
+++ train_interfaces.py
@@ -5,7 +5,7 @@ import torch
 
 def get_interface(ti_type, model, device, printer, **kwargs):
     if ti_type == MatchAndGoals.name:
-        return MatchAndGoals(model, device, printer)
+        return MatchAndGoals(model, device, printer, **kwargs)
     raise ValueError(f"Unknown ti_type: {ti_type}")
 
 
@@ -25,6 +25,7 @@ class MatchAndGoals(ITrainInterface):
         self.d = device
         self.alpha = float(kwargs.get('alpha', [1.0])[0])
         self.beta = float(kwargs.get('beta', [1.0])[0])
+        self.c0 = float(kwargs.get('c0', [100.0])[0])
 
     def train_step(self, sample):
         teams, goals = sample[0].to(self.d), sample[1].to(self.d)
@@ -35,27 +36,29 @@ class MatchAndGoals(ITrainInterface):
         p1 = pred[:, 0]
         p2 = pred[:, 1]
 
-        win = 100. * torch.relu(p1 - p2)
-        draw = -200. * torch.abs(p1 - p2) + 100.
-        loose = 100. * torch.relu(p2 - p1)
+        win = self.c0 * torch.relu(p1 - p2)
+        draw = -2. * self.c0 * torch.abs(p1 - p2) + self.c0
+        loose = self.c0 * torch.relu(p2 - p1)
 
         pred = torch.softmax(torch.stack([win, draw, loose], -1), -1)
         # to indices
         targets = torch.stack([
-            goals1 > goals2, goals1 == goals1, goals2 > goals1
+            goals1 > goals2, goals1 == goals2, goals2 > goals1
         ], -1).long().max(-1)[1]
 
         xent = self.xent_loss(pred, targets)
         mse_1 = self.mse(goals1.float(), p1)
         mse_2 = self.mse(goals2.float(), p2)
         loss = self.alpha * xent + self.beta * .5 * (mse_1 + mse_2)
-        #loss = self.alpha * xent
+
+        abs_1 = torch.abs(goals1.float() - p1).mean().item()
+        abs_2 = torch.abs(goals2.float() - p2).mean().item()
 
         assert not bool(torch.isnan(loss))
 
         metrics = {
             'mse': .5 * (mse_1 + mse_2).item(), 'xent': xent.item(),
-            'loss': loss.item()
+            'abs': .5 * (abs_1 + abs_2)
         }
         counters = None
         functions = {
